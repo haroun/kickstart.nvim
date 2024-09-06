620deee3cc548acfd5e5277dfb2531a44bf27175
Harouna Traoré <haroun@users.noreply.github.com>
2024-09-07 00:01:28 +0200

    fix tsserver renaming

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -232,6 +232,7 @@ return {
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
+          server_name = server_name == 'tsserver' and 'ts_ls' or server_name
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
